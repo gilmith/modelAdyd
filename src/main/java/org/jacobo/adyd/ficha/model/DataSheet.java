069a10a611c87207bb7a7cf5873b865631bcc44c
JacoboGeadaAnsino <jacobo.geada.ansino@gmail.com>
2025-03-08 23:59:59 +0100

    version estable de la ficha

--- src/main/java/org/jacobo/adyd/ficha/model/DataSheet.java
+++ src/main/java/org/jacobo/adyd/ficha/model/DataSheet.java
@@ -1,10 +1,11 @@
 package org.jacobo.adyd.ficha.model;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-public record DataSheet(String name, String playerName, String playerEmail,  Race raza, String alignment,
+public record DataSheet(String id, String name, String playerName, String playerEmail,  Race raza, String alignment,
 		Clase clase, Integer level, Integer gac0, Integer hitPoints, String gender, 
 		Integer ac, List<String> languages, Proficiencies proficiencies, List<Equipment> equipment,
-		Habilities habilities) {
+		Habilities habilities, LocalDateTime creationDate, LocalDateTime modificationDate) {
 
 }
